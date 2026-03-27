4af5adcb029759a01768dd518f57b773c7e208be
Nuno Marujo <ablutecompany@gmail.com>
2026-03-27 01:11:19 +0000

    fix: allow all external image sources and update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: '*.unsplash.com',
-      },
+      { protocol: 'https', hostname: '**' },
     ],
   },
 }
